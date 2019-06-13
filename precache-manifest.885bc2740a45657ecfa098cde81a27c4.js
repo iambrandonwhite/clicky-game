28ca59cdff9c4ccce1c2b803e875b7965f8698b9
Brandon White <brandonwhite4118@gmail.com>
2019-06-12 23:15:54 -0700

    Updates

---
+++ precache-manifest.885bc2740a45657ecfa098cde81a27c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c42198b8335d1bdc116",
+    "url": "/clicky-game/static/css/main.dcb75d66.chunk.css"
+  },
+  {
+    "revision": "2c42198b8335d1bdc116",
+    "url": "/clicky-game/static/js/main.f19f4689.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "b7b5116bdb0f15147d58e13290595a81",
+    "url": "/clicky-game/index.html"
+  }
+];
